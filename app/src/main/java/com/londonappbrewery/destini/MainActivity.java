e4f94fe15fd28b66ffa7df94780e29f2b2c34c2f
harinmehta <harinmehta17@gmail.com>
2019-04-19 02:09:37 +0530

    Created my logic. I don't think i could save data on orientation using this logic.Will have to create index variable for that

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,17 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    private Button topButton;
+    private Button bottomButton;
+    private TextView storyText;
 
 
     @Override
@@ -15,16 +22,60 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        topButton = findViewById(R.id.buttonTop);
+        bottomButton = findViewById(R.id.buttonBottom);
+        storyText = findViewById(R.id.storyTextView);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(storyText.getText() == getString( R.string.T1_Story) || (storyText.getText() == (getString(R.string.T2_Story)))){
+
+                    Log.d("Destini","Top button clicked, showing T3");
+                    storyText.setText(R.string.T3_Story);
+                    topButton.setText(R.string.T3_Ans1);
+                    bottomButton.setText(R.string.T3_Ans2);
 
 
+                }else {
 
-
+                    Log.d("Destini","Top button clicked, showing T6");
+                    storyText.setText(R.string.T6_End);
+                    topButton.setVisibility(View.GONE);
+                    bottomButton.setText(R.string.Exit);
+                }
+            }
+        });
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (storyText.getText() == ((getString(R.string.T1_Story)))) {
 
+                    Log.d("Destini","Bottom button clicked, showing T2");
+                    bottomButton.setText(R.string.T2_Ans2);
+                    topButton.setText(R.string.T2_Ans1);
+                    storyText.setText(R.string.T2_Story);
+
+                } else if (storyText.getText() == (getString(R.string.T2_Story))) {
+
+                    Log.d("Destini","Bottom button clicked, showing T4");
+                    storyText.setText(R.string.T4_End);
+
+                } else if (storyText.getText() == (getString(R.string.T3_Story)) && (bottomButton.getText() != (getString(R.string.Exit)))) {
+
+                    Log.d("Destini","Bottom button clicked, showing T5");
+                    storyText.setText(R.string.T5_End);
+                    topButton.setVisibility(View.GONE);
+                    bottomButton.setText(R.string.Exit);
+                } else{
+                    finish();
+                }
+            }
+        });
     }
 }
