1ca5866f4ae8d7f10de9b45dd4d770afda78608e
harinmehta <harinmehta17@gmail.com>
2019-04-19 12:23:23 +0530

    Index thing worked!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button topButton;
     private Button bottomButton;
     private TextView storyText;
+    private int mStoryIndex = 1;
 
 
     @Override
@@ -21,23 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
         topButton = findViewById(R.id.buttonTop);
         bottomButton = findViewById(R.id.buttonBottom);
         storyText = findViewById(R.id.storyTextView);
 
+        if(savedInstanceState != null){
+            mStoryIndex = savedInstanceState.getInt("IndexKey");
+        }else{
+            mStoryIndex = 1;
+        }
+
+        if(mStoryIndex == 5 || mStoryIndex == 6){
+            topButton.setVisibility(View.GONE);
+        }
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
         topButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(storyText.getText() == getString( R.string.T1_Story) || (storyText.getText() == (getString(R.string.T2_Story)))){
+                if(mStoryIndex == 1 || mStoryIndex == 2){
 
                     Log.d("Destini","Top button clicked, showing T3");
                     storyText.setText(R.string.T3_Story);
                     topButton.setText(R.string.T3_Ans1);
                     bottomButton.setText(R.string.T3_Ans2);
+                    mStoryIndex = 3;
 
 
                 }else {
@@ -46,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                     storyText.setText(R.string.T6_End);
                     topButton.setVisibility(View.GONE);
                     bottomButton.setText(R.string.Exit);
+                    mStoryIndex = 6;
                 }
             }
         });
@@ -54,28 +68,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (storyText.getText() == ((getString(R.string.T1_Story)))) {
+                if (mStoryIndex == 1) {
 
                     Log.d("Destini","Bottom button clicked, showing T2");
                     bottomButton.setText(R.string.T2_Ans2);
                     topButton.setText(R.string.T2_Ans1);
                     storyText.setText(R.string.T2_Story);
+                    mStoryIndex = 2;
 
-                } else if (storyText.getText() == (getString(R.string.T2_Story))) {
+                } else if (mStoryIndex == 2) {
 
                     Log.d("Destini","Bottom button clicked, showing T4");
                     storyText.setText(R.string.T4_End);
+                    topButton.setVisibility(View.GONE);
+                    bottomButton.setText(R.string.Exit);
+                    mStoryIndex = 4;
 
-                } else if (storyText.getText() == (getString(R.string.T3_Story)) && (bottomButton.getText() != (getString(R.string.Exit)))) {
+                } else if (mStoryIndex == 3) {
 
                     Log.d("Destini","Bottom button clicked, showing T5");
                     storyText.setText(R.string.T5_End);
                     topButton.setVisibility(View.GONE);
                     bottomButton.setText(R.string.Exit);
+                    mStoryIndex = 5;
+
                 } else{
                     finish();
                 }
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outstate){
+
+        super.onSaveInstanceState(outstate);
+        outstate.putInt("IndexKey",mStoryIndex);
+
     }
 }
